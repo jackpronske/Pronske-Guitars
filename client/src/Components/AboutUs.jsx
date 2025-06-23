ec1240a42f20474e76ee4d4a1a4fb11f1c7c7ddc
Jack Pronske <jackpronske@gmail.com>
2025-06-22 22:19:20 -0500

    adding more fade logic

--- client/src/Components/AboutUs.jsx
+++ client/src/Components/AboutUs.jsx
@@ -5,7 +5,7 @@ import "../Styles/AboutUs.scss";
 export default function AboutUs() {
   return (
     <div className="body-container">
-      <div className="body-section main">
+      <div className="body-section fade main">
         <div className="main-about-text-container">
           <div className="main-about-text-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
           <div className="main-about-text-subtext">Vivamus egestas ante vitae ultrices maximus. Praesent efficitur finibus ullamcorper. Etiam sit amet nisi aliquam ante mattis vulputate. Sed cursus ullamcorper tortor vel fermentum. Sed eget diam varius, laoreet arcu in, ullamcorper libero. Fusce congue nisl ac arcu faucibus dictum. Aenean vestibulum neque accumsan mi lobortis iaculis. Curabitur orci sapien, laoreet at nisi vel, pretium finibus mauris.</div>
@@ -16,10 +16,10 @@ export default function AboutUs() {
           </div>
         </div>
       </div>
-      <div className="body-section cta">
-        <div className="about-cta">Proin sed mi ex. Proin nec eleifend nibh. Nunc lobortis semper lorem, vel luctus lectus tincidunt non.</div>
+      <div className="body-section fade cta">
+        <div className="about-cta fade">Proin sed mi ex. Proin nec eleifend nibh. Nunc lobortis semper lorem, vel luctus lectus tincidunt non.</div>
       </div>
-      <div className="body-section full-image">
+      <div className="body-section fade full-image">
         <div className="about-full-image">
           <img src={'https://picsum.photos/seed/1/1500/1200'} alt='full image about' />
           <div className="overlay-text">Family owned. Family run.<br /> Handcrafted, quality instruments.</div>
--- server/routes/form.js
+++ server/routes/form.js
@@ -6,7 +6,7 @@ const sgMail = require('../utilities/sendgrid');
 router.get('/', (req, res) => {
   console.log('form get hit');
   const msg = {
-    to: 'pronske@yahoo.com',
+    to: 'carolinepronske@gmail.com',
     from: 'newsletter@pronskeguitars.com',
     subject: 'Pronske Guitars Newsletter - June Edition',
     text: 'We have a great newsletter for you today! Thank you so much for being a part of our company, we love you! -Jack',
