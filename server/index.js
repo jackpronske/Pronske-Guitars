b02e38f3c7ac04b089464752bd66be3b962bdb2d
Jack Pronske <jackpronske@gmail.com>
2025-10-13 20:56:01 -0500

    final commit before first deploy

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const path = require("path");
 const app = express();
-const port = 3030;
+const port = process.env.PORT || 10000;
 
 const formRouter = require('./routes/form.js');
 
